c04374e8843105d830fb963249d26e5e1360a1d1
FelixRN <felixsr023@gmail.com>
2024-11-19 13:39:25 +0100

    Nuevo ejercicio

---
+++ Programacion_ejercicios/Ejercicio016/Main.java
@@ -0,0 +1,26 @@
+package Ejercicio016;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double dateF = //declaración de la variable;
+    System.out.println("Esto es un convertidor de temperatura");
+    System.out.println("Ingresa un número con decimales: ");
+
+    Scanner scanner = new Scanner(System.in);
+    //Capturar el número de entrada
+    
+        double number = scanner.nextDouble();
+        String date1 = scanner.next();
+        System.out.println("¿En que unidad lo quieres cambiar, grados o farenheit?");
+        String date = scanner.next();
+        //System.out.println(date.contains("grados"));
+
+        if (date.equals("grados")) {
+            
+        }else { 
+            System.err.println(dateF - dateF);
+        }
+    }
+}

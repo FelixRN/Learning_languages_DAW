2f7ecf30acb4dd384b6e7d70cba947e64e4a1cff
Felix <felixsr023@gmail.com>
2024-11-20 00:15:23 +0100

    Ejercicio calculadora programacion

--- Programacion_ejercicios/Ejercicio016/Main.java
+++ Programacion_ejercicios/Ejercicio016/Main.java
@@ -1,26 +1,33 @@
-package Ejercicio016;
-
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Main {
     public static void main(String[] args) {
-        double dateF = //declaración de la variable;
+        double dateF; //declaración de la variable;
+        
     System.out.println("Esto es un convertidor de temperatura");
     System.out.println("Ingresa un número con decimales: ");
 
     Scanner scanner = new Scanner(System.in);
     //Capturar el número de entrada
-    
-        double number = scanner.nextDouble();
-        String date1 = scanner.next();
+    dateF = scanner.nextDouble();
+
+    //Creamos el formato a decimal
+    DecimalFormat df = new DecimalFormat("#.00");
         System.out.println("¿En que unidad lo quieres cambiar, grados o farenheit?");
         String date = scanner.next();
-        //System.out.println(date.contains("grados"));
+        
 
-        if (date.equals("grados")) {
+        if (date.equalsIgnoreCase("grados")) {
+            double celsius = (dateF - 32) * 5 / 9;
+            System.out.println("La temperatura en grados Celsius es: " + df.format(celsius));
             
-        }else { 
-            System.err.println(dateF - dateF);
+        }else if (date.equalsIgnoreCase("farenheit")) {
+            double fahrenheit = (dateF * 9 / 5) + 32;
+            System.out.println("La temperatura en Farenheit es: " + df.format(fahrenheit));
+        } else{
+            System.err.println("Unidad no reconocida");
         }
+        scanner.close();
     }
 }

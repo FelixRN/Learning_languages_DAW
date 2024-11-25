465a57c3ce090cb6e58d532bc6dea6f96a2f6c15
FelixRN <felixsr023@gmail.com>
2024-11-25 14:10:09 +0100

    ejercicios

---
+++ Programacion/Ejercicios_Clase/Ejercicio017/Main.java
@@ -0,0 +1,11 @@
+package Ejercicios_Clase.Ejercicio017;
+
+public class Main {
+    public static void main(String[] args) {
+        int num = 5;
+
+        int factorial = MathUtils.factorial(num);
+        System.out.println("El factorial de " + num + "es " + factorial);
+    }
+}
+

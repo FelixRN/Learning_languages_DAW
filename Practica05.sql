df7759cba09003ba5afb14c7c50ca41a0bdce669
Felix <felixsr023@gmail.com>
2024-12-11 21:57:28 +0100

    SQL

--- Practica05.sql
+++ Practica05.sql
@@ -10,22 +10,23 @@ direccion Varchar(50)
 );
 
 Create table paciente(
-direccion Varchar(50)
+direccion Varchar(50),
 Dni_paciente Varchar(10) primary key,
-Dni_medico Varchar(10) foreign key 
-Nombre_pac Varchar(30) Not null
-Dir_pac Varchar(50)
-Telefono Varchar(10)
+Dni_medico Varchar(10),
+Nombre_pac Varchar(30) Not null,
+Dir_pac Varchar(50),
+Telefono Varchar(10),
+foreign key(Dni_medico) references paciente(Dni_paciente) 
 ); 
 
 Create table trabaja(
-DNI_medico Varchar(10)
-Id_sala Varchar(10)
-Horario Varchar(40)
+DNI_medico Varchar(10) primary key,
+Id_sala Int primary key,
+Horario Varchar(40) Not null
 );
 
 Create table Salas(
-Id_sala INT
-Ubicación Varchar(50) 
+Id_sala INT primary key,
+Ubicacion Varchar(50) Not null
 );
 
